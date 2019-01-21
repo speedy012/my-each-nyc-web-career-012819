2f3b3633d7216f44bccf51d968036909280d46d8
Herman <hrivas5726@aol.com>
2019-01-21 19:36:53 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,8 +1,9 @@
-def my_each(word) # put argument(s) here
-  # code here
-      collection = [1, 2, 3, 4]
-    my_each(collection) do |i|
-       i
-    end
+
+def my_each(collection)
+  i = 0
+  while i < collection.length
+    yield(collection[i])
+    i = i + 1
   end
+  collection
 end
